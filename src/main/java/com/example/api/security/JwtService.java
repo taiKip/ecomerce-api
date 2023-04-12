117582b488b34b2d0b341ce10db406e60114a939
tarus <victortarus6@gmail.com>
2023-04-12 17:32:43 +0300

    Added environment variables for jwt-key,aws-access-key,aws-secret-access-key

--- src/main/java/com/example/api/security/JwtService.java
+++ src/main/java/com/example/api/security/JwtService.java
@@ -17,7 +17,7 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-    @Value("${jwt.secret-key}")
+    @Value("${jwt.jwt-key}")
     private  String SECRET_KEY;
 
     public String extractUsername(String token) {
